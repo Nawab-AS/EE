3c87b07d96105746abbaf963d2168dab16842ffe
Nawab-AS <59747708+Nawab-AS@users.noreply.github.com>
2025-11-25 14:26:26 -0500

    non-tutorial learning :)

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,39 @@ use std::cmp::Ordering;
 
 
 fn main() {
-    println!("Guess the number!");
+    print!("\nGuess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let mut secret_number: u16 = 0;
+    loop {
+        println!("\nWhat should the maximum range be?");
+
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        
+        secret_number = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                if input.trim().to_lowercase() == "quit" {
+                    println!("Thank you for playing");
+                    break;
+                }
+                println!("Please type a number!");
+                continue;
+            }
+        };
+
+        if secret_number >= 2 {
+            break;
+        }
+    }
+
+    let secret_number = rand::thread_rng().gen_range(1..=secret_number);
 
     loop {
-        println!("\nPlease input your guess.");
+        println!("\nPlease input your guess");
 
         let mut guess = String::new();
 
@@ -17,10 +44,13 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        println!("You guessed: {guess}");
-        let guess: u32 = match guess.trim().parse() {
+        let guess: u16 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
+                if guess.trim().to_lowercase() == "quit" {
+                    println!("Thank you for playing");
+                    break;
+                }
                 println!("Please type a number!");
                 continue;
             }
