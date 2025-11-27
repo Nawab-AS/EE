698fcc3b8ede14aa77cb3c566733bf49651ccad4
Nawab-AS <59747708+Nawab-AS@users.noreply.github.com>
2025-11-27 11:31:03 -0500

    learning control structures...

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,32 @@
 use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
-
 
 fn main() {
-    print!("\nGuess the number!");
-
-    let mut secret_number: u16 = 0;
-    loop {
-        println!("\nWhat should the maximum range be?");
-
+    println!("Generate the nth fibonacci number");
+    let n: u8 = loop {
         let mut input = String::new();
 
         io::stdin()
             .read_line(&mut input)
             .expect("Failed to read line");
         
-        secret_number = match input.trim().parse() {
-            Ok(num) => num,
+        match input.trim().parse() {
+            Ok(num) => if num > 0 && num < 186 { break num } else {println!("Number out of range")},
             Err(_) => {
-                if input.trim().to_lowercase() == "quit" {
-                    println!("Thank you for playing");
-                    break;
-                }
                 println!("Please type a number!");
-                continue;
             }
         };
+    };
 
-        if secret_number >= 2 {
-            break;
-        }
-    }
+    println!("generating the {n}th fibonacci number");
+    println!("The {n}th fibonacci number is {}", fibonacci(n));
+}
 
-    let secret_number = rand::thread_rng().gen_range(1..=secret_number);
+fn fibonacci(n: u8) -> u128{
+    let mut nums: [u128; 2] = [1, 1];
 
-    loop {
-        println!("\nPlease input your guess");
+    for _i in 0..(n-1) {
+        nums = [nums[1], nums[0]+nums[1]];
+    };
 
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u16 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                if guess.trim().to_lowercase() == "quit" {
-                    println!("Thank you for playing");
-                    break;
-                }
-                println!("Please type a number!");
-                continue;
-            }
-        };
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        };
-    }
+    nums[0]
 }
